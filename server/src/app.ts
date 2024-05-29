23251699f80682c954a1d6d32f9403557c1036fb
cedricahenkorah <cedricahenkorah@gmail.com>
2024-05-29 14:23:58 +0000

    user router

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import morgan from "morgan";
+import userRouter from "./users/users.router";
 import { corsOptions } from "./config/corsOptions";
 import cors from "cors";
 
@@ -11,5 +12,6 @@ app.use(morgan("dev"));
 app.get("/", (req: Request, res: Response) => {
   res.send("faceview server is live");
 });
+app.use("/users", userRouter);
 
 export default app;

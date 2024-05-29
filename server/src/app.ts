a23e1bddd8516d7f0dab2968e190468e2dbc30df
cedricahenkorah <cedricahenkorah@gmail.com>
2024-05-29 14:21:03 +0000

    cors set up

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
 import morgan from "morgan";
+import { corsOptions } from "./config/corsOptions";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
---
+++ server/src/config/corsOptions.ts
@@ -0,0 +1,11 @@
+import { allowedOrigins } from "./allowedOrigins";
+
+export const corsOptions = {
+  origin: (origin: any, callback: any) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};

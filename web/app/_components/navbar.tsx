d9657d651be63350fad23f733fbb1688ac96a381
cedricahenkorah <cedricahenkorah@gmail.com>
2024-07-24 15:28:13 +0000

    style: redesigned the landing page

---
+++ web/app/_components/navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <header className="sticky top-0 z-50 bg-white">
+      <nav className="w-full ">
+        <div className="m-auto lg:max-w-7xl max-w-3xl px-5 lg:px-5 xl:px-0 py-3 lg:py-5 flex justify-between">
+          <div className="flex items-center">
+            <Link href="/">
+              <h1 className="font-semibold text-lg">faceview</h1>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,6 +2,10 @@
 
 import { useRouter } from "next/navigation";
 import { v1 as uuid } from "uuid";
+import { Navbar } from "./_components/navbar";
+import { Button } from "@/components/ui/button";
+import { Video } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   const router = useRouter();
@@ -12,10 +16,37 @@ export default function Home() {
   }
 
   return (
-    <div className="min-h-screen p-20">
-      <button className="bg-black text-white p-2" onClick={create}>
-        create room
-      </button>
+    <div className="min-h-screen">
+      <section className="min-h-screen">
+        <Navbar />
+
+        <div className="relative isolate px-6 pt-10 lg:px-8">
+          <div className="mx-auto max-w-3xl">
+            <div className="text-center">
+              <h1 className="text-xl font-semibold tracking-tight text-gray-900 sm:text-6xl">
+                Stay Connected with FaceView
+              </h1>
+
+              <p className="mt-2 lg:mt-4 text-sm lg:text-lg lg:leading-8 text-gray-500 font-semibold">
+                Connect, converse, and collaborate with people everywhere using
+                FaceView.
+              </p>
+            </div>
+
+            <div className="flex mt-10 gap-x-3">
+              <Button onClick={create}>
+                <Video className="mr-2 h-4 w-4" />
+                New meeting
+              </Button>
+
+              <Input type="text" placeholder="Enter meeting link" />
+              <Button variant="secondary" disabled>
+                Join
+              </Button>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }

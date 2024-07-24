35dd3c8769e1a6269c1d7494797880f2e9690622
cedricahenkorah <cedricahenkorah@gmail.com>
2024-07-24 11:54:29 +0000

    feat: create room page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,24 +1,20 @@
-import { Navbar } from "./_components/navbar";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { v1 as uuid } from "uuid";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen">
-      <Navbar />
-      <div className="absolute inset-0 -z-10 h-full w-full items-center [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-        <div className="relative isolate px-6 pt-14 lg:px-8">
-          <div className="mx-auto max-w-6xl py-10 sm:py-14 lg:py-24">
-            <div className="text-center">
-              <h1 className="text-2xl font-semibold tracking-tight text-white sm:text-5xl md:leading-relaxed">
-                Stay Connected with FaceView
-              </h1>
+  const router = useRouter();
 
-              <p className="mt-2 lg:mt-4 text-sm lg:text-lg lg:leading-8 text-white font-semibold">
-                Connect, converse, and collaborate with people everywhere using
-                FaceView.
-              </p>
-            </div>
-          </div>
-        </div>
+  function create() {
+    const id = uuid();
+    router.push(`/room/${id}`);
+  }
+
+  return (
+    <div className="min-h-screen flex">
+      <div className="flex-grow flex justify-center items-center">
+        <button onClick={create}>create room</button>
       </div>
     </div>
   );

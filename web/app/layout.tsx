ea67a956987a6e4d57745c81ba636a30f6f5bbb5
cedricahenkorah <cedricahenkorah@gmail.com>
2024-07-25 08:23:54 +0000

    refactor: removed next auth provider and toaster

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Provider } from "@/providers/Provider";
 import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,12 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Provider>
-          <Toaster />
-          {children}
-        </Provider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }

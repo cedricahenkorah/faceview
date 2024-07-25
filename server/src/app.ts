6febdc23543a82188f52f36b4495a68e264be9fb
cedricahenkorah <cedricahenkorah@gmail.com>
2024-07-25 09:08:46 +0000

    refactor: socket connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,7 @@
 import express, { Request, Response } from "express";
 import morgan from "morgan";
-import userRouter from "./users/users.router";
 import { corsOptions } from "./config/corsOptions";
 import cors from "cors";
-import authRouter from "./auth/auth.router";
 
 const app = express();
 
@@ -15,8 +13,5 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
   res.send("faceview server is live");
 });
-
-app.use("/users", userRouter);
-app.use("/auth", authRouter);
 
 export default app;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
-import "dotenv/config";
 import http from "http";
 import app from "./app";
 import { Server } from "socket.io";
-import { connectDB } from "./config/dbConnection";
-import mongoose from "mongoose";
+import cors from "cors";
 
-const PORT = process.env.PORT || 7002;
+app.use(cors());
+
+const PORT = 7002;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
@@ -14,76 +14,40 @@ const io = new Server(server, {
   },
 });
 
-interface Users {
-  [roomID: string]: string[];
-}
-
-interface SocketToRoom {
-  [socketID: string]: string;
-}
-
-const users: Users = {};
-const socketToRoom: SocketToRoom = {};
+const rooms: any = {};
 
 io.on("connection", (socket) => {
   socket.on("join room", (roomID) => {
-    if (users[roomID]) {
-      const length = users[roomID].length;
-
-      if (length === 4) {
-        socket.emit("room full");
-        return;
-      }
-
-      users[roomID].push(socket.id);
+    if (rooms[roomID]) {
+      rooms[roomID].push(socket.id);
     } else {
-      users[roomID] = [socket.id];
+      rooms[roomID] = [socket.id];
     }
 
-    socketToRoom[socket.id] = roomID;
-    const usersInThisRoom = users[roomID].filter((id) => id !== socket.id);
+    const anotherUser = rooms[roomID].find((id: any) => id !== socket.id);
 
-    socket.emit("all users", usersInThisRoom);
-  });
-
-  socket.on("sending signal", (payload) => {
-    io.to(payload.userToSignal).emit("user joined", {
-      signal: payload.signal,
-      callerID: payload.callerID,
-    });
-  });
-
-  socket.on("returning signal", (payload) => {
-    io.to(payload.callerID).emit("receiving returned signal", {
-      signal: payload.signal,
-      id: socket.id,
-    });
-  });
-
-  socket.on("disconnect", () => {
-    const roomID = socketToRoom[socket.id];
-    let room = users[roomID];
-    if (room) {
-      room = room.filter((id) => id !== socket.id);
-      users[roomID] = room;
+    if (anotherUser) {
+      socket.emit("other user", anotherUser);
+      socket.to(anotherUser).emit("user joined", socket.id);
+      console.log("joined");
     }
   });
-});
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready");
-});
-
-mongoose.connection.on("error", (err: any) => {
-  console.log("MongoDB connection error", err);
-});
-
-async function startServer() {
-  connectDB();
-
-  server.listen(PORT, () => {
-    console.log(`Faceview is live on port ${PORT}`);
+  socket.on("offer", (payload) => {
+    io.to(payload.target).emit("offer", payload);
   });
-}
 
-startServer();
+  socket.on("answer", (payload) => {
+    io.to(payload.target).emit("answer", payload);
+  });
+
+  socket.on("ice-candidate", (incoming) => {
+    io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`faceview is live on port ${PORT}`);
+});
+
+// startServer();
